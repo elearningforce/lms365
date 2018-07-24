02e56e598ac7c94109c0cad98daf422d83308a4a
Aleksei Gus <aleksei.gus@elearningforce.com>
2018-07-24 16:57:32 +0300

    ability to execute sync requests

--- src/query-executer.ts
+++ src/query-executer.ts
@@ -6,6 +6,7 @@ export enum HttpMethod {
 }
 
 export interface Query {
+    async?: boolean;
     contentType?: string | boolean;
     data?: any;
     ignoreProcessData?: boolean;
@@ -17,4 +18,5 @@ export interface Query {
 export interface QueryExecuter {
     execute<T>(query: Query): Promise<T>;
     execute<TInput, TOutput>(query: Query, convert?: (input: TInput) => TOutput): Promise<TOutput>;
+    executeSync<TOutput>(query: Query, converter?: (input: string) => TOutput): TOutput;
 }
